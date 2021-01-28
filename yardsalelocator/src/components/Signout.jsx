9ddfd3070a427dcc6f76c1099d8593de26091109
Raymond Albright <67028365+razer523@users.noreply.github.com>
2021-01-27 16:54:48 -0800

    added signout refresh

--- yardsalelocator/src/components/Signout.jsx
+++ yardsalelocator/src/components/Signout.jsx
@@ -5,6 +5,7 @@ import { Redirect } from 'react-router-dom'
 export class Signout extends Component {
     componentDidMount() {
         window.localStorage.clear();
+        window.location.reload();
         }
 
     render() {
