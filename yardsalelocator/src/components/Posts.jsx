ed72a14018fc34a01bb2dab25b2c458409385be2
JulienCruz06 <miranda9@pnw.edu>
2021-02-13 17:13:24 -0600

    Card for Posts (still editing)

--- yardsalelocator/src/components/Posts.jsx
+++ yardsalelocator/src/components/Posts.jsx
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
-import ReactTable from "react-table";
+import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
 import moment from "moment";
 import "react-table/react-table.css";
+
 
 export default class Posts extends Component {
 	constructor(props) {
@@ -46,85 +47,55 @@ export default class Posts extends Component {
 	}
 
 
+	postsListings() {
+		let post = this.state.post;
+		for (var  i = 0; i <post.length; i++){
+			return (
+				<Card className="text-center" border="primary">
+					<Card.Header>{this.state.post[i].id}</Card.Header>
+					<Card.Body>
+					  <Card.Title>Yard Sale</Card.Title>
+					  <Card.Text id="address">
+						  {this.state.post[i].streetAddress}, {this.state.post[i].city}, {this.state.post[i].state}, {this.state.post[i].zip}
+					  </Card.Text>
+					  <Card.Subtitle className="mb-2 text-muted">Starting Date   |   Ending Date</Card.Subtitle>
+					  <Card.Text id="startEndDate">
+						  {this.state.post[i].startDate}       {this.state.post[i].endDate}
+					  </Card.Text>
+					  <Card.Subtitle className="mb-2 text-muted">Starting Time   |   Ending Time</Card.Subtitle>
+					  <Card.Text id="startEndTime">
+						  {this.state.post[i].startTime}       {this.state.post[i].endTime}
+					  </Card.Text>
+					  <Card.Link href="/editPost">Edit Post   </Card.Link> 
+					<Card.Link href="#">   Delet Post</Card.Link>
+					</Card.Body>
+				  	<Card.Footer id="categories">{this.state.post[i].categories}</Card.Footer>
+			  	</Card> 
+			)
+		}
+	}
+
+
 	render() {
-		const columns = [
-			{
-				Header: "ID",
-				accessor: "id",
-				width: '40'
-			},
-			{
-				Header: "Street Address",
-				accessor: "streetAddress",
-				width: 'auto',
-				style: { 'whiteSpace': 'unset' }
-			},
-			{
-				Header: "City",
-				accessor: "city",
-				width: 'auto'
-			},
-			{
-				Header: "State",
-				accessor: "state",
-				width: 'auto'
-			},
-			{
-				Header: "Zip",
-				accessor: "zip",
-				width: 'auto'
-			},
-			{
-				Header: "Starting Date",
-				id: "startDate",
-				accessor: (a) => <Fragment>{moment(a.startDate).format("MMM Do YYYY")}</Fragment>,
-				width: 'auto'
-			},
-			{
-				Header: "Ending Date",
-				id: "endDate",
-				accessor: (a) => <Fragment>{moment(a.endDate).format("MMM Do YYYY")}</Fragment>,
-				width: 'auto'
-			},
-			{
-				Header: "Start Time",
-				id: "startTime",
-				accessor: (a) => <Fragment>{moment(a.startTime).format("h:mm:ss a")}</Fragment>,
-				width: 'auto'
-			},
-			{
-				Header: "End Time",
-				id: "endTime",
-				accessor: (a) => <Fragment>{moment(a.endTime).format("h:mm:ss a")}</Fragment>,
-				width: 'auto'
-			},
-			{
-				Header: "Categories",
-				accessor: "categories",
-				width: 'auto',
-				style: { 'whiteSpace': 'unset' }
-			},
-		];
+		
 		return (
 			<>
-				{" "}
 				<h1>Yard Sale Postings</h1>
-				<div style={{ width: "80vw" }}>
-					<ReactTable data={this.state.post} columns={columns} defaultPageSize={10} />
-					<Button>
+				<Button>
 						<Link to="/createpost">Create New Post </Link>
-					</Button>
-				</div>
+				</Button>
+				{this.postsListings()}
+				
 			</>
 		);
 	}
 }
 
-//Fetch example from Darrin Deal Video https://vimeo.com/showcase/7506077/video/395976709
-// constructor(props) {
-//   super(props);
+{/* {/* //Fetch example from Darrin Deal Video https://vimeo.com/showcase/7506077/video/395976709
+// constructor(props) { */}
+{/* //   super(props);
 
-//   this.state = {
+//   this.state = { */}
 //     posts: []
 //   }
 // }
@@ -148,3 +119,113 @@ export default class Posts extends Component {
 // )
 
 //fetch info site: developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch   to post, button uses a fecth command, needed: URL, method, and body.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div >
+								<h1>{this.state.post[i].id}</h1><br />
+								<h4>id</h4>
+								<p id="id">{this.state.post[i].id}</p>
+								<div>
+									<h4>Address</h4>
+									<p id="address">{this.state.post[i].streetAddress}</p>
+									<p id="city">{this.state.post[i].city} {this.state.post[i].state} {this.state.post[i].zip}</p>
+								</div>
+								<div>
+									<h4>Starting Date</h4>
+									<p id="startDate">{this.state.post[i].startDate}</p>
+								</div>
+								<div>
+									<h4>Endning Date</h4>
+									<p id="endDate">{this.state.post[i].endDate}</p>
+								</div>
+								<div>
+									<h4>Start Time</h4>
+									<p id="startTime">{this.state.post[i].startTime}</p>
+								</div>
+								<div>
+									<h4>End Time</h4>
+									<p id="endTime">{this.state.post[i].endTime}</p>
+								</div>
+								<div>
+									<h4>Categories</h4>
+									<p id="categories">{this.state.post[i].categories}</p>
+								</div>
+				 				<button><Link to="/editPost">Edit Profile</Link></button>
+						</div> */}
+
+						
+
+
+
+
+
+
+// <div >
+// 							<div >
+// 								<h1>{this.state.post[i].id}</h1><br />
+// 								<h4>id</h4>
+// 								<p id="id">{this.state.post[i].id}</p>
+// 								<div>
+// 									<h4>Address</h4>
+// 									<p id="address">{this.state.post[i].streetAddress}</p>
+// 									<p id="city">{this.state.post[i].city} {this.state.post[i].state} {this.state.post[i].zip}</p>
+// 								</div>
+// 								<div>
+// 									<h4>Starting Date</h4>
+// 									<p id="startDate">{this.state.post[i].startDate}</p>
+// 								</div>
+// 								<div>
+// 									<h4>Endning Date</h4>
+// 									<p id="endDate">{this.state.post[i].endDate}</p>
+// 								</div>
+// 								<div>
+// 									<h4>Start Time</h4>
+// 									<p id="startTime">{this.state.post[i].startTime}</p>
+// 								</div>
+// 								<div>
+// 									<h4>End Time</h4>
+// 									<p id="endTime">{this.state.post[i].endTime}</p>
+// 								</div>
+// 								<div>
+// 									<h4>Categories</h4>
+// 									<p id="categories">{this.state.post[i].categories}</p>
+// 								</div>
+// 				 				<button><Link to="/editPost">Edit Profile</Link></button>
+// 						</div>
+		
+{/* <Card className="text-center">
+					<Card.Header>{this.state.post[i].id}</Card.Header>
+					<Card.Body>
+					  <Card.Title>Yard Sale</Card.Title>
+					  <Card.Text id="address">
+						  {this.state.post[i].streetAddress} + "," + {this.state.post[i].city} + "," {this.state.post[i].state} + "," {this.state.post[i].zip}
+					  </Card.Text>
+					  <Card.Subtitle className="mb-2 text-muted">Starting Date   |   Ending Date</Card.Subtitle>
+					  <Card.Text id="startEndDate">
+						  {this.state.post[i].startDate}       {this.state.post[i].endDate}
+					  </Card.Text>
+					  <Card.Subtitle className="mb-2 text-muted">Starting Time   |   Ending Time</Card.Subtitle>
+					  <Card.Text id="startEndTime">
+						  {this.state.post[i].startTime}       {this.state.post[i].endTime}
+					  </Card.Text>
+					  <Button variant="primary"><Card.Link href="/editPost">Edit Post</Card.Link><Go somewhere</Button>
+					  <Button variant="primary"><Card.Link href="#">Delet Post</Card.Link><Go somewhere</Button>
+					</Card.Body>
+				  <Card.Footer id="categories">{this.state.post[i].categories}</Card.Footer>
+			  </Card> */}
